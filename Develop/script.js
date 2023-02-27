b0d1e682c5deab6af8b737388d386b71f0b58043
YeaItsJesus <122085651+YeaItsJesus@users.noreply.github.com>
2023-02-26 19:45:43 -0600

    working on getting user inputs to save in Local Storage

--- Develop/script.js
+++ Develop/script.js
@@ -3,13 +3,12 @@
 // in the html.
 
 $(function () {
-  
+ 
   //Sets the current date on top of page
 //TODO add ordinal to day of month
 let todaysDate = dayjs().format('dddd, MMMM, D')
   $('#currentDay').text(todaysDate);
 
-  
   let currentTime = dayjs().hour();
 
   //Interval refreshes and runs code every minute
@@ -39,129 +38,45 @@ $.each ( thisText, function (indexes, values) {
     }
 
 
+  
+  
 
 })
 
+  }
+
+});
+
+
+function setAtNine () {
+
+  //Variables for local storage execution
+let nineAm = document.getElementById("9").value;
+let tenAm = document.getElementById("10").value;
+let elevenAm = document.getElementById("11").value;
+let twelveAm = document.getElementById("12").value;
+let onePm = document.getElementById("13").value;
+let twoPm = document.getElementById("14").value;
+let threePm = document.getElementById("15").value;
+let fourPm = document.getElementById("16").value;
+let fivePm = document.getElementById("17").value;
+let nineButton = document.getElementById("nineBtn");
+
+
+
+ 
+  document.getElementById("nineBtn").addEventListener("click", function ()
+  {
+      var atNine = document.getElementById("9").value ;
+     
+      localStorage.setItem("9AM", atNine) ;
+      alert("Good Timing") ;
+  } , false);
+  
 }
 
+setAtNine()
 
-
-
-
-
- 
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
-  // let timeBlock = $(".time-block");
-  //Loop
-  // $(timeBlock).each(function() {
-  //   let timeBlockHour = parseInt($
-  //     (this).attr('id').split('-')[1]);
-
-
-  //     if (timeBlockHour < currentTime) {
-  //       $(this).addClass("past");
-  //     }
-
-  //     else if ( timeBlockHour === currentTime ) {
-  //       $(this).addClass("present");
-  //     }
-  //     else {
-  //       $(this).addClass("future");
-  //     }
-
-  // })
-
-
-
-
-  
-
-  
-  
-
-
- 
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
-});
 
 // Acceptance Criteria 
 
