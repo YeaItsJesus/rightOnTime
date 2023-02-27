107c57af427bbca2064cb332e3de9b63cc8a724e
YeaItsJesus <122085651+YeaItsJesus@users.noreply.github.com>
2023-02-26 21:34:52 -0600

    update

--- Develop/script.js
+++ Develop/script.js
@@ -2,6 +2,11 @@
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 
+
+
+
+
+
 $(function () {
  
   //Sets the current date on top of page
@@ -48,34 +53,119 @@ $.each ( thisText, function (indexes, values) {
 });
 
 
-function setAtNine () {
 
-  //Variables for local storage execution
-let nineAm = document.getElementById("9").value;
-let tenAm = document.getElementById("10").value;
-let elevenAm = document.getElementById("11").value;
-let twelveAm = document.getElementById("12").value;
-let onePm = document.getElementById("13").value;
-let twoPm = document.getElementById("14").value;
-let threePm = document.getElementById("15").value;
-let fourPm = document.getElementById("16").value;
-let fivePm = document.getElementById("17").value;
-let nineButton = document.getElementById("nineBtn");
+
+
+//This function will save user inputs to local storage and ale
+function saveToLocalStorage () {
+
+ 
 
 
 
  
   document.getElementById("nineBtn").addEventListener("click", function ()
   {
+    
       var atNine = document.getElementById("9").value ;
      
       localStorage.setItem("9AM", atNine) ;
+      
+      
       alert("Good Timing") ;
   } , false);
   
+
+  document.getElementById("tenBtn").addEventListener("click", function ()
+  {
+      var atTen = document.getElementById("10").value ;
+     
+      localStorage.setItem("10AM", atTen) ;
+      alert("Sounds like a Plan.") ;
+  } , false);
+  
+
+  document.getElementById("elevenBtn").addEventListener("click", function ()
+  {
+      var atEleven = document.getElementById("11").value ;
+     
+      localStorage.setItem("11AM", atEleven) ;
+      alert("What better to do than this?") ;
+  } , false);
+  
+
+  document.getElementById("twelveBtn").addEventListener("click", function ()
+  {
+      var atTwelve = document.getElementById("12").value ;
+     
+      localStorage.setItem("12PM", atTwelve) ;
+      alert("What a time to be Alive") ;
+  } , false);
+  
+
+  document.getElementById("oneBtn").addEventListener("click", function ()
+  {
+      var atOne = document.getElementById("13").value ;
+     
+      localStorage.setItem("1PM", atOne) ;
+      alert("It is all Set") ;
+  } , false);
+  
+
+  document.getElementById("twoBtn").addEventListener("click", function ()
+  {
+      var atTwo = document.getElementById("14").value ;
+     
+      localStorage.setItem("2PM", atTwo) ;
+      alert("Do not be late") ;
+  } , false);
+  
+
+  document.getElementById("threeBtn").addEventListener("click", function ()
+  {
+      var atThree = document.getElementById("15").value ;
+     
+      localStorage.setItem("3PM", atThree) ;
+      alert("Do not Give Up") ;
+  } , false);
+  
+
+  document.getElementById("fourBtn").addEventListener("click", function ()
+  {
+      var atFour = document.getElementById("16").value ;
+     
+      localStorage.setItem("4PM", atFour) ;
+      alert("You got it.") ;
+  } , false);
+  
+
+  document.getElementById("fiveBtn").addEventListener("click", function ()
+  {
+      var atFive = document.getElementById("17").value ;
+     
+      localStorage.setItem("5PM", atFive) ;
+      alert("Right on Time") ;
+  } , false);
+  
+
+  
+
+
+
+
+
 }
 
-setAtNine()
+saveToLocalStorage()
+
+
+
+
+
+
+
+
+
 
 
 // Acceptance Criteria 
